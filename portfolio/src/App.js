1b501af31a2ec5a17085de197096c38a89d3897f
Jiahao <a32112354jhj@gamil.com>
2020-08-19 18:08:25 +0800

    fix:專案初始

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/header.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+
+function Header(){
+    return(
+        <>
+            <div>
+                Header
+            </div>
+        </>
+    );
+}
+
+export default Header;
